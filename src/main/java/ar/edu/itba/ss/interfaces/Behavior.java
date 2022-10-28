5cc25c0d9aca8105bb3230f4ed3add5abd487d61
Roberto J Catalan <rcatalan@itba.edu.ar>
2022-10-28 18:07:47 -0300

    feat: radius added to Persona & impl Behavior interface

--- src/main/java/ar/edu/itba/ss/interfaces/Behavior.java
+++ src/main/java/ar/edu/itba/ss/interfaces/Behavior.java
@@ -2,4 +2,7 @@ package ar.edu.itba.ss.interfaces;
 
 public interface Behavior {
 
+    void calculateTarget();
+    void calculateVelocity();
+    void elude();
 }
--- src/main/java/ar/edu/itba/ss/models/Person.java
+++ src/main/java/ar/edu/itba/ss/models/Person.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.ss.models;
 
+import ar.edu.itba.ss.interfaces.Behavior;
 import ar.edu.itba.ss.interfaces.Movable;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,25 @@ public class Person implements Movable {
 
     @Getter
     private Velocity velocity;
+
+    @Setter
+    @Getter
+    private Behavior behavior;
+
+    @Getter
+    @Setter
+    private double radius;
+
+    private final static double MAX_RADIUS = 0.32;
+
+    private final static double MIN_RADIUS = 0.15;
+
+    public Person(Vector position, Velocity velocity, Behavior behavior, double radius) {
+        this.position = position;
+        this.velocity = velocity;
+        this.behavior = behavior;
+        this.radius = radius;
+    }
 
     @Override
     public void move(double dt) {
