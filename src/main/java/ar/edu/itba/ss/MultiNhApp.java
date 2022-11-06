09ed1eb090c32ba9ffe8c4602d0946b190b6580f
Julian Arce <juarce@itba.edu.ar>
2022-11-06 11:53:57 -0300

    feat: run multiple times each nh

--- src/main/java/ar/edu/itba/ss/MultiNhApp.java
+++ src/main/java/ar/edu/itba/ss/MultiNhApp.java
@@ -10,38 +10,41 @@ import ar.edu.itba.ss.utils.VelocityExporter;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static ar.edu.itba.ss.models.Person.MAX_RADIUS;
 import static ar.edu.itba.ss.utils.Random.getRandom;
 
 public class MultiNhApp {
     public static void main(String[] args) {
-        List<Integer> humansQty = List.of(2, 10, 40, 80, 140, 200, 260, 320);
+        final List<Integer> humansQty = List.of(2, 10, 40, 80, 140, 200, 260, 320);
+        final int tries = 5;
 
         humansQty.stream().parallel().forEach(hQty -> {
-            final List<Vector> positions = PositionReader.readPositions(String.format("src/main/resources/input/positions_%s.txt", hQty));
-            final List<Person> entities = positions.stream().map(p -> new Person(p, new Velocity(HumanBehavior.V_WANDER, getRandom(0, 2 * Math.PI)), PersonState.HUMAN, MAX_RADIUS)).collect(Collectors.toList());
-            entities.add(new Person(new Vector(0, 0), new Velocity(HumanBehavior.V_WANDER, getRandom(0, 2 * Math.PI)), PersonState.ZOMBIE,  MAX_RADIUS));
+            IntStream.range(0, tries).forEach(i -> {
+                final List<Vector> positions = PositionReader.readPositions(String.format("src/main/resources/input/positions_%s.txt", hQty));
+                final List<Person> entities = positions.stream().map(p -> new Person(p, new Velocity(HumanBehavior.V_WANDER, getRandom(0, 2 * Math.PI)), PersonState.HUMAN, MAX_RADIUS)).collect(Collectors.toList());
+                entities.add(new Person(new Vector(0, 0), new Velocity(HumanBehavior.V_WANDER, getRandom(0, 2 * Math.PI)), PersonState.ZOMBIE, MAX_RADIUS));
 
-            final double dt = Person.MIN_RADIUS / (2 * ZombieBehavior.VD_MAX);
-            final double beta = 0.9;
-            final double tau = 0.5;
+                final double dt = Person.MIN_RADIUS / (2 * ZombieBehavior.VD_MAX);
+                final double beta = 0.9;
+                final double tau = 0.5;
 
-            System.out.println("Simulation with " + hQty + " humans");
-            System.out.println("dt: " + dt);
+                System.out.println("Simulation " + i + " with " + hQty + " humans");
+                System.out.println("dt: " + dt);
 
-            Exporter csvExporter = new CsvExporter("output/ovito/", String.format("output_%s.csv", hQty));
-            csvExporter.open();
-            Exporter velocityExporter = new VelocityExporter("output/velocity/", String.format("velocity_%s.csv", hQty));
-            velocityExporter.open();
+                Exporter csvExporter = new CsvExporter("output/ovito/", String.format("output_%s_%d.csv", hQty, i));
+                csvExporter.open();
+                Exporter velocityExporter = new VelocityExporter("output/velocity/", String.format("velocity_%s_%d.csv", hQty, i));
+                velocityExporter.open();
 
-            final Simulator system = new Simulator(entities, dt, beta, tau, csvExporter, velocityExporter);
-            system.simulate();
+                final Simulator system = new Simulator(entities, dt, beta, tau, csvExporter, velocityExporter);
+                system.simulate();
 
-            csvExporter.close();
-            velocityExporter.close();
-            System.out.println("Simulation with " + hQty + " humans finished");
-
+                csvExporter.close();
+                velocityExporter.close();
+                System.out.println("Simulation " + i + " with " + hQty + " humans finished");
+            });
         });
     }
 }
